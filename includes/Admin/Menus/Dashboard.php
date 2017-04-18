2ec70ea65d8fe1f4e6a8de347e28f53248761cff
klhall1987@gmail.com <klhall1987@gmail.com>
2017-04-18 14:01:50 -0400

    Add dashboard tempaltes.

--- includes/Admin/Menus/Dashboard.php
+++ includes/Admin/Menus/Dashboard.php
@@ -28,41 +28,7 @@ final class NF_Admin_Menus_Dashboard extends NF_Abstracts_Submenu
         wp_enqueue_script( 'backbone-radio', Ninja_Forms::$url . 'assets/js/lib/backbone.radio.min.js', array( 'jquery', 'backbone' ) );
         wp_enqueue_script( 'backbone-marionette-3', Ninja_Forms::$url . 'assets/js/lib/backbone.marionette3.min.js', array( 'jquery', 'backbone' ) );
         wp_enqueue_script( 'nf-dashboard', Ninja_Forms::$url . 'assets/js/min/dashboard.min.js', array( 'backbone-radio', 'backbone-marionette-3' ) );
-
-        // TODO: Move to a template file.
-        ?>
-        <div class="wrap">
-            <div id="ninja-forms-dashboard"></div>
-        </div>
-        <script id="tmpl-nf-dashboard" type="text/template">
-            <h1>Ninja Forms Dashboard</h1>
-            <nav>
-                <ul>
-                    <li class="widgets">Forms</li>
-                    <li class="apps">Apps</li>
-                    <li class="memberships">Membership</li>
-                </ul>
-            </nav>
-            <div class="content"></div>
-        </script>
-        <script id="tmpl-nf-content" type="text/template">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid cupiditate ducimus fugit illo itaque maxime nihil perferendis praesentium voluptates. Aperiam culpa delectus distinctio illo ipsum officia, officiis pariatur quasi.</p>
-        </script>
-        <script id="tmpl-nf-widget-forms" type="text/template">
-            [FORMS WIDGET]
-        </script>
-        <script id="tmpl-nf-widgets" type="text/template">
-            <div class="widget-forms"></div>
-        </script>
-        <script id="tmpl-nf-apps" type="text/template">
-            [APPS HERE]
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, adipisci aut dolor doloribus ducimus expedita fuga itaque maiores mollitia neque nostrum, nulla odit praesentium quaerat reiciendis repellat sint velit veniam!</p>
-        </script>
-        <script id="tmpl-nf-memberships" type="text/template">
-            [MEMBERSHIPS HERE]
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, dolore illum molestiae quis reprehenderit sequi. Commodi consequatur doloremque itaque obcaecati odit officia optio. Autem ducimus excepturi iusto minima molestias provident.</p>
-        </script>
-        <?php
+        Ninja_Forms::template( 'admin-menu-dashboard.html.php' );
     }
 
 } // End Class NF_Admin_Menus_Dashboard
---
+++ includes/Templates/admin-menu-dashboard.html.php
@@ -0,0 +1,87 @@
+<!-- Dashboard Wrap -->
+<div class="wrap">
+    <div id="ninja-forms-dashboard"></div>
+</div>
+
+
+
+<!-- Auth Notice -->
+<script id="tmpl-nf-auth-notice" type="text/template">
+    [AUTH GOES HERE]
+    <div id="auth-notice"></div>
+</script>
+
+<!-- Navigation Template -->
+<script id="tmpl-nf-dashboard" type="text/template">
+    <h1>Ninja Forms Dashboard</h1>
+    <div class="notices"></div>
+    <nav>
+        <ul>
+            <li class="widgets">Forms</li>
+            <li class="apps">Apps</li>
+            <li class="memberships">Membership</li>
+        </ul>
+    </nav>
+    <div class="content"></div>
+</script>
+
+<!-- Content Template -->
+<script id="tmpl-nf-content" type="text/template">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid cupiditate ducimus fugit illo itaque maxime nihil perferendis praesentium voluptates. Aperiam culpa delectus distinctio illo ipsum officia, officiis pariatur quasi.</p>
+</script>
+
+<!-- Form Widget -->
+<script id="tmpl-nf-widget-forms" type="text/template">
+    [FORMS WIDGET]
+    <div class="action"></div>
+    <div class="filter"></div>
+    <div class="content"></div>
+    <div class="action2"></div>
+</script>
+
+<!-- Widget Area Template -->
+<script id="tmpl-nf-widgets" type="text/template">
+    <div class="widget-forms"></div>
+</script>
+
+<!-- Forms Template -->
+<script id="tmpl-nf-forms-template" type="text/template">
+</script>
+
+<!-- Widget Forms Table -->
+<script id="tmpl-nf-widget-forms-table" type="text/template">
+    <table>
+        <thead>
+            <tr>
+                <th><?php _e( 'Title', 'ninja-forms' ); ?></th>
+                <th><?php _e( 'Shortcode', 'ninja-forms' ); ?></th>
+                <th><?php _e( 'Date Created', 'ninja-forms' ); ?></th>
+            </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </table>
+</script>
+
+<script id="tmpl-nf-widget-forms-table-row" type="text/template">
+    <tr>
+        <td>{{{ data.title }}}</td>
+        <td>{{{ data.shortcode }}}</td>
+        <td>{{{ data.date-created }}}</td>
+    </tr>
+</script>
+
+<!-- Apps Content -->
+<script id="tmpl-nf-apps" type="text/template">
+    <!-- TODO: Create static template -->
+    [APPS HERE]
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, adipisci aut dolor doloribus ducimus expedita fuga itaque maiores mollitia neque nostrum, nulla odit praesentium quaerat reiciendis repellat sint velit veniam!</p>
+</script>
+
+<!-- Membership Content -->
+<script id="tmpl-nf-memberships" type="text/template">
+    <!-- TODO: Create static template -->
+    [MEMBERSHIPS HERE]
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, dolore illum molestiae quis reprehenderit sequi. Commodi consequatur doloremque itaque obcaecati odit officia optio. Autem ducimus excepturi iusto minima molestias provident.</p>
+</script>
