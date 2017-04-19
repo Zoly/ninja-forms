52a15ef6cd906279fedae162412e89218c5cca22
Kyle B. Johnson <kbjohnson90@gmail.com>
2017-04-19 14:14:09 -0400

    add forms table empty view.

--- client/dashboard/views/widgets/formsTableBody.js
+++ client/dashboard/views/widgets/formsTableBody.js
@@ -6,13 +6,13 @@
  * @copyright (c) 2017 WP Ninjas
  * @since 3.2
  */
-define( [ 'views/widgets/formsTableRow', 'models/formCollection' ], function( TableRow, FormCollection ) {
+define( [ 'views/widgets/formsTableRow', 'views/widgets/formsTableEmpty', 'models/formCollection' ], function( TableRowView, EmptyView, FormCollection ) {
     var view = Marionette.CollectionView.extend( {
         template: "#tmpl-nf-widget-forms-table",
         collection: new FormCollection(),
-        childView: TableRow,
+        childView: TableRowView,
+        emptyView: EmptyView,
         tagName: 'tbody',
-        
     } );
     return view;
 } );
